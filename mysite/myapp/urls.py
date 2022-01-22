5934b1a3aa9d575416294b650c7f16dd1f50c4b9
NoumanShah042 <noumanrehman042@gmail.com>
2022-01-22 23:57:24 +0500

    views + models + database

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,7 +1,11 @@
-from django.urls import  path
+from django.urls import path
 
 from . import views
 
-urlpatterns =[
-    path('', views.index, name="index")
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('index2/', views.index2, name="index2"),
+    path('index3/', views.index3, name="index3"),
+    path('posts/', views.displayPosts, name="posts"),
+    path('user/<int:id>', views.getUserById, name="getUser")
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,8 +1,90 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from datetime import datetime
+from .models import Post
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("hello")
+
+
+def getUserById(request, id):
+    user = User.objects.get(id=id)
+    profile = {
+        "name": user.username,
+        "email": user.email
+    }
+    return JsonResponse(profile)
+
+
+def index2(request):
+    return render(request, "myapp/index.html")
+
+
+def index3(request):
+    # Strings
+    my_name = "Nouman"
+
+    # Integers
+    my_age = 30
+
+    # html string
+    my_html = "<h1>This is some HTML</h1>"
+
+    # Lists
+    langs = ["Python", "JavaScript", "Bash", "Ruby", "C", "Rust"]
+
+    res = [['Adam', '334', 'Ford'], ['Steve', '130', 'Porsche'], ['Dave', '510', 'Ford']]
+
+    # Dictionaries
+    friends = {
+        "Ali": 43,
+        "Humza": 28,
+        "Syed": 26,
+        "Farhan": 23,
+        "Fatima": 39
+    }
+
+    # Tuples
+    colors = ("Red", "Blue")
+
+    # Booleans
+    bool_var = True
+
+    # Classes
+    class GitRemote:
+        def __init__(self, name, description, domain):
+            self.name = name
+            self.description = description
+            self.domain = domain
+
+        def clone(self, repo):
+            return f"Cloning into {repo}"
+
+    my_remote = GitRemote(
+        name="Learning Flask",
+        description="Learn the Flask web framework for Python",
+        domain="https://github.com/Julian-Nash/learning-flask.git"
+    )
+
+    # Functions
+    def repeat(x, qty=1):
+        return x * qty
+
+    date = datetime.utcnow()
+
+    suspicious = "<script>alert('NEVER TRUST USER INPUT!')</script>"
+    context = {'langs': langs, "res": res, "var1": my_name, "my_age": my_age, "friends": friends, "colors": colors,
+               "bool_var": bool_var, "GitRemote": GitRemote, "my_remote": my_remote, "repeat": repeat,
+               "date": date, "my_html": my_html, "suspicious": suspicious
+               }
+    return render(request, "myapp/gettingDataFromViews.html", context)
+
+
+def displayPosts(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'myapp/posts.html', context)
